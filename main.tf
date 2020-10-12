c4fc80115001beb0520802ea6d79542b26abb1b5
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-10-12 16:09:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ provider "aws" {
   }
 
 resource "aws_instance" "web" {
-  ami = "ami-666c8317"
+#  ami = "ami-666c8317"
+  ami = "ami=0856ab1d46b4e2ce1" 
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
